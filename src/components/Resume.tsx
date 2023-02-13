0bc787ce36da4bf325945be5dc729cf7c8115018
KoridzeG <giorgiqori@yahoo.com>
2023-02-13 22:55:38 +0400

    feat : last UI update resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -15,30 +15,44 @@ const Resume: React.FC<ResumeProps> = ({ formData }) => {
 
 
     return (
-        <Box display='flex' height='100%' gap='1.5rem' flexDirection='column' paddingLeft='6em' paddingTop='3.5em' sx={{ whiteSpace:'pre-wrap' }} >
+        <Box display='flex' height='100%' gap='1.5rem' flexDirection='column' paddingLeft='6em' paddingTop='3.5em' sx={{ whiteSpace: 'pre-wrap' }} maxWidth='45%' >
             {formData.image && (
                 <img src={formData.image} alt="" style={{ width: '246px', height: '246px', position: 'absolute', left: '82%', borderRadius: '133px' }} />
             )}
-            <Box display='flex' gap='1.5em' maxWidth='80%' flexDirection='row'>
-                <Typography color='#F93B1D' fontWeight='bold' fontSize='36px' maxWidth='40%' style={{ wordWrap: "break-word" }}>{formData.name}</Typography>
-                <Typography color='#F93B1D' fontWeight='700' fontSize='36px' maxWidth='40%'  style={{ wordWrap: "break-word" }}>{formData.surname}</Typography>
+            <Box display='flex' gap='1.5em' maxWidth='100%' flexDirection='row'>
+                <Typography color='#F93B1D' fontWeight='bold' fontSize='36px' maxWidth='60%' style={{ wordWrap: "break-word" }}>{formData.name}</Typography>
+                <Typography color='#F93B1D' fontWeight='700' fontSize='36px' maxWidth='60%' style={{ wordWrap: "break-word" }}>{formData.surname}</Typography>
             </Box>
             <Box display='flex' paddingLeft='0.5em' flexDirection='column' gap='1rem'>
                 <Box display='flex' gap='1rem' flexDirection='row' alignItems='center' maxWidth='65%'>
-                    <img style={{ width: '20px', height: '20px' }} src={mail} alt="" />
-                    <Typography fontSize='20px' maxWidth='65%' style={{ wordWrap: "break-word" }}>{formData.email}</Typography>
+                    {formData.email && (
+                        <>
+                            <img style={{ width: '20px', height: '20px' }} src={mail} alt="" />
+                            <Typography fontSize='20px' maxWidth='65%' style={{ wordWrap: "break-word" }}>{formData.email}</Typography>
+                        </>
+                    )}
                 </Box>
                 <Box display='flex' gap='1rem' flexDirection='row' alignItems='center'>
-                    <img style={{ width: '20px', height: '20px' }} src={phone} alt="" />
-                    <Typography fontSize='20px' maxWidth='45%' style={{ wordWrap: "break-word" }}>{formData.phone_number}</Typography>
+                    {formData.phone_number && (
+                        <>
+                            <img style={{ width: '20px', height: '20px' }} src={phone} alt="" />
+                            <Typography fontSize='20px' maxWidth='45%' style={{ wordWrap: "break-word" }}>{formData.phone_number}</Typography>
+                        </>
+                    )}
                 </Box>
             </Box>
-            <Typography color='#F93B1D' fontWeight='700' fontSize='28px'>ჩემ შესახებ</Typography>
+            {formData.about_me && (
+                <Typography color='#F93B1D' fontWeight='700' fontSize='28px'>ჩემ შესახებ</Typography>
+            )}
             <Box display='flex' flexDirection='column' maxWidth='70%'>
                 <Typography fontSize='19px' fontWeight='400' maxWidth='100%' style={{ wordWrap: "break-word" }}>{formData.about_me}</Typography>
             </Box>
-            <img src={smallLine} alt="" style={{ maxWidth: '90%' }} />
-            <Typography fontWeight='700' fontSize='28px' color='#F93B1D'>გამოცდილება</Typography>
+            {formData.about_me && (
+                <img src={smallLine} alt="" style={{ maxWidth: '90%' }} />
+            )}
+            {formData.experiences.some(experience => experience.position) ? (
+                <Typography fontWeight='700' fontSize='28px' color='#F93B1D'>გამოცდილება</Typography>
+            ) : null}
             {formData.experiences.map((experience, index) => (
                 <React.Fragment key={index}>
                     <Box display='flex' flexDirection='row' gap='1em' maxWidth='45%'>
@@ -59,22 +73,22 @@ const Resume: React.FC<ResumeProps> = ({ formData }) => {
                     </Box>
                 </React.Fragment>
             ))}
-            <img src={smallLine} alt="" style={{ width: '90%' }} />
-            <Typography fontWeight='700' fontSize='28px' color='#F93B1D'>განათლება</Typography>
+            {formData.experiences.some(exp => exp.description) ? (<img src={smallLine} alt="" style={{ width: '90%' }} />) : null}
+            {formData.educations.some(edu => edu.institute) ? (<Typography fontWeight='700' fontSize='28px' color='#F93B1D'>განათლება</Typography>) : null}
             {formData.educations.map((edu, idx) => (
                 <React.Fragment key={idx}>
                     <Box display='flex' maxWidth='45%'>
-                    <Typography fontWeight='700' color='#1A1A1A' style={{ wordWrap: "break-word" }} >{edu.institute}</Typography>
+                        <Typography fontWeight='700' color='#1A1A1A' style={{ wordWrap: "break-word" }} >{edu.institute}</Typography>
                     </Box>
                     <Box display='flex' maxWidth='45%'>
-                        {edu.due_date ? new Date(edu.due_date).toISOString().substring(0,10) : ''} 
+                        {edu.due_date ? new Date(edu.due_date).toISOString().substring(0, 10) : ''}
                     </Box>
                     <Box display='flex' maxWidth='500px'>
-                    <Typography fontSize='16px' fontWeight='400' width='100%' style={{ wordWrap: "break-word" }}>{edu.description}</Typography>
+                        <Typography fontSize='16px' fontWeight='400' width='100%' style={{ wordWrap: "break-word" }}>{edu.description}</Typography>
                     </Box>
                 </React.Fragment>
             ))}
-            <img src={rlogo} alt="sadasdasdasdsadasdd" style={{position:'absolute',width:'40px',height:'40px',bottom:'5%',right:'39%'}} />
+            <img src={rlogo} alt="sadasdasdasdsadasdd" style={{ position: 'absolute', width: '40px', height: '40px', bottom: '5%', right: '39%' }} />
         </Box>
     )
 }
